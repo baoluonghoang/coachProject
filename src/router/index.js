4f86be39f464182cbca4cc26faf374c2e4103dd7
baolh <baolh@runsystem.net>
2021-03-05 10:20:47 -0800

    completed fix reviews

--- src/router/index.js
+++ src/router/index.js
@@ -6,62 +6,43 @@ const routes = [
   {
     path: "/coaches",
     name: "CoachList",
-    component: () =>
-      import(
-        /* webpackChunkName: "CoachList" */ "../views/coaches/CoachList.vue"
-      ),
+    component: () => import("../views/coaches/CoachList.vue"),
   },
   {
     path: "/coaches/:id",
     name: "CoachDetail",
-    component: () =>
-      import(
-        /* webpackChunkName: "CoachDetail" */ "../views/coaches/CoachDetail.vue"
-      ),
+    component: () => import("../views/coaches/CoachDetail.vue"),
     props: true,
     children: [
       {
         path: "contact",
         name: "CoachContact",
-        component: () =>
-          import(
-            /* webpackChunkName: "CoachContact" */ "../views/requests/CoachContact.vue"
-          ),
+        component: () => import("../views/requests/CoachContact.vue"),
       },
     ],
   },
   {
     path: "/register",
     name: "Register",
-    component: () =>
-      import(
-        /* webpackChunkName: "CoachRegister" */ "../views/coaches/CoachRegister.vue"
-      ),
+    component: () => import("../views/coaches/CoachRegister.vue"),
     meta: { authRequired: true },
   },
   {
     path: "/auth",
     name: "Auth",
-    component: () =>
-      import(/* webpackChunkName: "Auth" */ "../views/auth/Auth.vue"),
+    component: () => import("../views/auth/Auth.vue"),
     meta: { unAuthRequired: true },
   },
   {
     path: "/requests",
     name: "Requests",
-    component: () =>
-      import(
-        /* webpackChunkName: "CoachRequestsReceived" */ "../views/requests/CoachRequestsReceived.vue"
-      ),
+    component: () => import("../views/requests/CoachRequestsReceived.vue"),
   },
 
   {
     path: "/:NotFound(.*)*",
     name: "PageNotFound",
-    component: () =>
-      import(
-        /* webpackChunkName: "PageNotFound" */ "../components/layouts/PageNotFound.vue"
-      ),
+    component: () => import("../components/layouts/PageNotFound.vue"),
   },
 ];
 
